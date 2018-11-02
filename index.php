7ea2d0b1273f21e772fedf7cbb4453a87b1cf7c3
您 <dev-2018>
2018-11-02 07:31:39 +0000

    add

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
-var_dump($_SERVER);
+error_reporting(0);
+//var_dump($_SERVER);
+$conn = new mysqli('127.0.0.1',"coding",'coding');
+var_dump((array)$conn);
+if(!$conn){
+    exit($conn->error);
+}
+$result = $conn->query('SHOW DATABASES;');
+var_dump($result);
