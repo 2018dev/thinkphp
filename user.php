a439957aeaa84bde2da3513e03b78c3892467bd0
您 <dev-2018>
2018-11-12 09:32:49 +0000

    add user.php

---
+++ user.php
@@ -0,0 +1,13 @@
+<?php
+class user{
+    public function getOneUser($id){
+        if(!$id){
+            return false;
+        }
+    }
+    public function getBatchUserName($ids){
+        if(!$ids){
+            return fsle;
+        }
+    }
+}
