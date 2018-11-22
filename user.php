7909b99385494b6cb7cde1c96ec78c1e374d2db2
您 <dev-2018>
2018-11-22 02:23:20 +0000

    add file

--- user.php
+++ user.php
@@ -1,13 +1,16 @@
 <?php
 class user{
-    public function getOneUser($id){
+    public function getOneUser($id) {
         if(!$id){
             return false;
         }
     }
-    public function getBatchUserName($ids){
+    public function getBatchUserName($ids) {
         if(!$ids){
             return fsle;
         }
+    }
+    public function getUserList($where, $order, $group, $limit) {
+
     }
 }
